1f710251c13e1a42c118dec90f518e6a34331f7b
atavhare <atavhare@DIN52003509.corp.capgemini.com>
2016-08-26 10:39:51 +0530

    multiple line commits

--- GitPush1/src/test.java
+++ GitPush1/src/test.java
@@ -2,7 +2,9 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Github push new 21345675678");
+		System.out.println("Github push new 21345675678 latest");
+		System.out.println("Github push new 21345675678 latest");
+		System.out.println("Github push new 21345675678 latest");
 	}
 
 }
